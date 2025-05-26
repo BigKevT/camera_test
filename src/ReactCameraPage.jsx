9cbc45166fa9612feb6e71fd4e4bd9a276397a3a
DAT Kev <mingheng.tsai@gmail.com>
2025-05-26 18:20:27 +0800

    Your commit message here

--- src/ReactCameraPage.jsx
+++ src/ReactCameraPage.jsx
@@ -54,7 +54,7 @@ const ReactCameraPage = () => {
   // 拍照功能
   const capture = useCallback(() => {
     if (webcamRef.current) {
-      const imageSrc = webcamRef.current.getScreenshot({width: 1920, height: 1920});
+      const imageSrc = webcamRef.current.getScreenshot({width: 1080, height: 1080});
       setCapturedImage(imageSrc);
     }
   }, [webcamRef]);
